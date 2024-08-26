aefa6bc524485052f442fc501de36612af17d847
Tanya sahu <sahutanya26@gmail.com>
2024-08-26 13:32:51 +0530

    initial

---
+++ server.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const request=require("request");
+const bodyParser= require("body-parser");
+
+
+
+const app=express();
+
+app.use(express.static("assets"));
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname+"/about.html");
+  
+})
+
+app.get("/contact",(req,res)=>{
+    res.sendFile(__dirname+"/contact.html");
+})
+
+
+app.post("/contact",(req,res)=>{
+    var fname=req.body.FirstName;
+    var lname=req.body.LastName;
+    var email=req.body.Email;
+    var pnumber=req.body.PhoneNumber;
+    var text=req.body.Message;
+    
+    console.log(fname, lname,email,pnumber,text)
+
+    res.redirect("/")
+})
+
+
+
+app.listen(3000,function(req,res){
+    console.log ("started  server");
+})
+
+
